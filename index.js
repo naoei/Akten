7944fbae009032169f6ac63bf3ba3bcc8b13d6fa
naoei <altenyt@gmail.com>
2021-12-20 10:34:07 -0500

    Add level color collection

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 let Akten = require('./lib/Akten');
+const ColorChannel = require('./lib/structures/ColorChannel');
 let level = new Akten("level.gdx");
 
 level.metadata.options.FadesIn = true;
 level.metadata.options.FadesOut = true;
 
+level.colors.push(new ColorChannel({ id: 1, red: 255, green: 0, blue: 0 }));
+level.colors.push(new ColorChannel({ id: 2, red: 0, green: 255, blue: 0 }));
+level.colors.push(new ColorChannel({ id: 3, red: 0, green: 0, blue: 255 }));
+
 console.log(level.metadata.options._flags);
 console.log(level.metadata.options.Verified);
+
+console.log(level.colors);
 
 level._CreateWriteHeader();
